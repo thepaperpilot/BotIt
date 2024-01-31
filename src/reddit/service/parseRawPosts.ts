513e118c4c8d1e050e8fb3b584730cfd63ce1af6
thepaperpilot <thepaperpilot@gmail.com>
2024-01-31 11:52:49 +0000

    Put attribution line on posts without text descriptions as well

--- src/reddit/service/parseRawPosts.ts
+++ src/reddit/service/parseRawPosts.ts
@@ -9,9 +9,8 @@ export function parseRawPosts(rawPosts: RawPost[]): Post[] {
       url: rawPost.data.url_overridden_by_dest ?? rawPost.data.url,
     };
 
-    if (rawPost.data.selftext.length) {
-      post.content = (rawPost.data.selftext || '') + '\n\n*x-posted from [/r/' + rawPost.data.subreddit + '](https://old.reddit.com/r/' + rawPost.data.subreddit + '/comments/' + rawPost.data.id + ') by [/u/' + rawPost.data.author + '](https://old.reddit.com/u/' + rawPost.data.author + ')*';
-    }
+    post.content = (rawPost.data.selftext || '') + '\n\n*x-posted from [/r/' + rawPost.data.subreddit + '](https://old.reddit.com/r/' + rawPost.data.subreddit + '/comments/' + rawPost.data.id + ') by [/u/' + rawPost.data.author + '](https://old.reddit.com/u/' + rawPost.data.author + ')*';
+    post.content = post.content.replace(/#x200B;/g, '');
     return post;
   });
 
